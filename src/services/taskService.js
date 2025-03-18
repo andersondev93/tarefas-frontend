ebb84d1bb16e913dade52520f2eb10d465d54bc3
Anderson Gonçalves <anderson.dev93@gmail.com>
2025-03-18 20:09:28 -0300

    Primeiro commit

---
+++ src/services/taskService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL || "https://sua-api-backend.onrender.com/tasks";
+
+export const getTasks = async () => await axios.get(API_URL);
+
+export const createTask = async (task) => await axios.post(API_URL, task);
+
+export const updateTask = async (id, task) => await axios.put(`${API_URL}/${id}`, task);
+
+export const deleteTask = async (id) => await axios.delete(`${API_URL}/${id}`);
+
+export const completeTask = async (id) => {
+    return await axios.patch(`http://localhost:3001/tasks/${id}/complete`);
+};
+
+export const toggleTaskStatus = async (id) => {
+    return await axios.patch(`http://localhost:3001/tasks/${id}/toggle-status`);
+};
