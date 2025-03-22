f83f4fc04cf4303d8a7a57134a5932868a9ba1d6
Anderson Gonçalves <anderson.dev93@gmail.com>
2025-03-22 18:11:33 -0300

    corrigindo URL

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/tasks";
+const API_URL = "https://tarefas-backend-nb1b.onrender.com/tasks";
 
 export const getTasks = async () => await axios.get(API_URL);
 
